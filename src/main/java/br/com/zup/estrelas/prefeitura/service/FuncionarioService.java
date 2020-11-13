ebd236a6588ef729d6333306ad43b7cbeb3cb533
GrazielaRO <grazi375@hotmail.com>
2020-11-13 15:06:00 -0300

    tests

--- src/main/java/br/com/zup/estrelas/prefeitura/service/FuncionarioService.java
+++ src/main/java/br/com/zup/estrelas/prefeitura/service/FuncionarioService.java
@@ -16,8 +16,8 @@ import br.com.zup.estrelas.prefeitura.repository.FuncionarioRepository;
 import br.com.zup.estrelas.prefeitura.repository.SecretariaRepository;
 
 @Service
-public class FuncionarioService implements IFuncionarioService{
-	
+public class FuncionarioService implements IFuncionarioService {
+
 	private static final String SECRETARIA_INEXISTENTE = "A secretaria informada não existe.";
 	private static final String ALTERACAO_REALIZADA_COM_SUCESSO = "Alteração Realizada com sucesso!";
 	private static final String ORCAMENTO_FOLHA_PAGAMENTO_INSUFICIENTE = "Orçamento de folha de pagamento insuficiente!";
@@ -25,13 +25,13 @@ public class FuncionarioService implements IFuncionarioService{
 	private static final String FUNCIONARIO_JA_CADASTRADO = "Funcionário já cadastrado!";
 	private static final String FUNCIONARIO_REMOVIDO_COM_SUCESSO = "Funcionário removido com sucesso!";
 	private static final String FUNCIONARIO_INEXISTENTE = "Funcionário inexistente!";
-	
+
 	@Autowired
 	FuncionarioRepository funcionarioRepository;
-	
+
 	@Autowired
 	SecretariaRepository secretariaRepository;
-	
+
 	public MensagemDTO adicionaFuncionario(FuncionarioDTO funcionarioDto) {
 
 		if (funcionarioRepository.findByCpf(funcionarioDto.getCpf()).isPresent()) {
@@ -49,14 +49,14 @@ public class FuncionarioService implements IFuncionarioService{
 		Funcionario funcionario = new Funcionario();
 
 		BeanUtils.copyProperties(funcionarioDto, funcionario);
-		
+
 		funcionario.setDataAdmissao(LocalDate.now());
 		funcionario.setSecretaria(secretaria);
 
 		funcionarioRepository.save(funcionario);
 
 		secretaria.setOrcamentoFolha(secretaria.getOrcamentoFolha() - funcionario.getSalario());
-		
+
 		secretariaRepository.save(secretaria);
 
 		return new MensagemDTO(FUNCIONARIO_CADASTRADO_COM_SUCESSO);
@@ -68,37 +68,37 @@ public class FuncionarioService implements IFuncionarioService{
 
 	public MensagemDTO alteraFuncionario(Long idFuncionario, FuncionarioDTO funcionarioDto) {
 
-Optional<Funcionario> funcionarioConsultado = funcionarioRepository.findById(idFuncionario);
-		
+		Optional<Funcionario> funcionarioConsultado = funcionarioRepository.findById(idFuncionario);
+
 		if (funcionarioConsultado.isEmpty()) {
 			return new MensagemDTO(FUNCIONARIO_INEXISTENTE);
 		}
-		
+
 		Funcionario funcionario = funcionarioConsultado.get();
 		if (funcionarioDto.getIdSecretaria() != funcionario.getSecretaria().getIdSecretaria()) {
-			
+
 			Optional<Secretaria> secretariaConsultada = secretariaRepository.findById(funcionarioDto.getIdSecretaria());
 			if (secretariaConsultada.isEmpty()) {
 				return new MensagemDTO(SECRETARIA_INEXISTENTE);
 			}
-			
+
 			Secretaria secretaria = secretariaConsultada.get();
 			if (secretaria.getOrcamentoFolha() < funcionarioDto.getSalario()) {
 				return new MensagemDTO(ORCAMENTO_FOLHA_PAGAMENTO_INSUFICIENTE);
 			}
-			
+
 			Secretaria secretariaAntiga = funcionario.getSecretaria();
 			secretariaAntiga.setOrcamentoFolha(secretariaAntiga.getOrcamentoFolha() + funcionario.getSalario());
 			secretariaRepository.save(secretariaAntiga);
-			
+
 		}
-		
+
 		if (funcionarioDto.getSalario() >= funcionario.getSalario()) {
-		
+
 			if (funcionario.getSecretaria().getOrcamentoFolha() < funcionarioDto.getSalario()) {
 				return new MensagemDTO(ORCAMENTO_FOLHA_PAGAMENTO_INSUFICIENTE);
 			}
-		
+
 			Secretaria secretaria = secretariaRepository.findById(funcionarioDto.getIdSecretaria()).get();
 
 			funcionario.setNome(funcionarioDto.getNome());
@@ -107,31 +107,31 @@ Optional<Funcionario> funcionarioConsultado = funcionarioRepository.findById(idF
 			funcionario.setSecretaria(secretaria);
 			funcionario.setFuncao(funcionarioDto.getFuncao());
 			funcionario.setConcursado(funcionarioDto.getConcursado());
-			
+
 			secretaria.setOrcamentoFolha(secretaria.getOrcamentoFolha() - funcionario.getSalario());
 			secretariaRepository.save(secretaria);
 
-			funcionarioRepository.save(funcionario);	
+			funcionarioRepository.save(funcionario);
 		}
-		
+
 		return new MensagemDTO(ALTERACAO_REALIZADA_COM_SUCESSO);
 	}
 
 	public MensagemDTO removeFuncionario(Long idFuncionario) {
-		
+
 		if (!funcionarioRepository.existsById(idFuncionario)) {
-			
-			return new MensagemDTO (FUNCIONARIO_INEXISTENTE);
+
+			return new MensagemDTO(FUNCIONARIO_INEXISTENTE);
 		}
-		
+
 		Funcionario funcionario = funcionarioRepository.findById(idFuncionario).get();
 		Secretaria secretaria = secretariaRepository.findById(funcionario.getSecretaria().getIdSecretaria()).get();
 		secretaria.setOrcamentoFolha(secretaria.getOrcamentoFolha() + funcionario.getSalario());
 		secretariaRepository.save(secretaria);
 
 		funcionarioRepository.deleteById(idFuncionario);
-		
-		return new MensagemDTO (FUNCIONARIO_REMOVIDO_COM_SUCESSO);
+
+		return new MensagemDTO(FUNCIONARIO_REMOVIDO_COM_SUCESSO);
 	}
 
 	public List<Funcionario> listaFuncionarios() {
---
+++ src/test/java/br/com/zup/estrelas/prefeitura/service/SecretariaServiceTests.java
@@ -0,0 +1,114 @@
+//package br.com.zup.estrelas.prefeitura.service;
+//
+//import java.util.Optional;
+//
+//import org.junit.Assert;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.Mockito;
+//import org.mockito.junit.MockitoJUnitRunner;
+//
+//import br.com.zup.estrelas.prefeitura.dto.CriaSecretariaDTO;
+//import br.com.zup.estrelas.prefeitura.dto.MensagemDTO;
+//import br.com.zup.estrelas.prefeitura.entity.Secretaria;
+//import br.com.zup.estrelas.prefeitura.enums.AreaAtuacao;
+//import br.com.zup.estrelas.prefeitura.repository.SecretariaRepository;
+//
+//@RunWith(MockitoJUnitRunner.class)
+//public class SecretariaServiceTests {
+//	
+//	private static final String SECRETARIA_REMOVIDA_COM_SUCESSO = "Secretaria removida com sucesso";
+//	private static final String CADASTRO_REALIZADO_COM_SUCESSO = "Cadastro realizado com sucesso!";
+//	private static final String SECRETARIA_INEXISTENTE = "Secretaria inexistente.";
+//
+//	@Mock
+//	SecretariaRepository secretariaRepository;
+//	
+//	@InjectMocks
+//	SecretariaService secretariaService;
+//	
+//	@Test
+//	public void deveAdicionarSecretaria() {
+//		
+//		CriaSecretariaDTO secretariaDto = criaSecretariaDTO();
+//		
+//		Mockito.when(secretariaRepository.findByAreaAtuacao(AreaAtuacao.SAUDE).thenReturn(false));
+//		
+//		MensagemDTO mensagemRetornada = this.secretariaService.adicionaSecretaria(secretariaDto);
+//		MensagemDTO mensagemEsperada = new MensagemDTO (CADASTRO_REALIZADO_COM_SUCESSO);
+//		
+//		Assert.assertEquals("Deve retornar mensagem de sucesso no cadastro", mensagemEsperada, mensagemRetornada);
+//	}
+//	
+//	@Test
+//	public void naoDeveAdicionarSecretaria() {
+//
+//		CriaSecretariaDTO secretariaDto = criaSecretariaDTO();
+//
+//		Mockito.when(secretariaRepository.findByAreaAtuacao(AreaAtuacao.SAUDE).thenReturn(true));
+//
+//		MensagemDTO mensagemRetornada = this.secretariaService.adicionaSecretaria(secretariaDto);
+//		MensagemDTO mensagemEsperada = new MensagemDTO(CADASTRO_REALIZADO_COM_SUCESSO);
+//
+//		Assert.assertEquals("Deve retornar mensagem de sucesso no cadastro", mensagemEsperada, mensagemRetornada);
+//
+//	}
+//	
+//	@Test
+//    public void deveRemoverUmaSecretaria() {
+//		
+//		Mockito.when(secretariaRepository.findById(1L).thenReturn(true));
+//		
+//		MensagemDTO mensagemRetornada = this.secretariaService.removeSecretaria(1L);
+//		MensagemDTO mensagemEsperada = new MensagemDTO (SECRETARIA_REMOVIDA_COM_SUCESSO);
+//		
+//		Assert.assertEquals("Deve retornar mensagem de sucesso na remoção da secretaria", mensagemEsperada, mensagemRetornada);
+//		
+//	}
+//	
+//	@Test
+//    public void naoDeveRemoverUmaSecretaria() {
+//		
+//		Mockito.when(secretariaRepository.findById(1L).thenReturn(false));
+//		
+//		MensagemDTO mensagemRetornada = this.secretariaService.removeSecretaria(1L);
+//		MensagemDTO mensagemEsperada = new MensagemDTO (SECRETARIA_INEXISTENTE);
+//		
+//		Assert.assertEquals("Deve retornar mensagem insucesso na remoção da secretaria", mensagemEsperada, mensagemRetornada);
+//	}
+//	
+//
+//	static CriaSecretariaDTO criaSecretariaDTO() {
+//
+//		CriaSecretariaDTO secretariaDto = new CriaSecretariaDTO();
+//
+//		secretariaDto.setAreaAtuacao(AreaAtuacao.SAUDE);
+//		secretariaDto.setOrcamentoProjeto(1000.0);
+//		secretariaDto.setOrcamentoFolha(1000.0);
+//		secretariaDto.setTelefone("34965847562");
+//		secretariaDto.setEndereco("Rua das Ostras, 123");
+//		secretariaDto.setSite("www.msmg.gov.br");
+//		secretariaDto.setEmail("msmg@gov.br");
+//		
+//		return secretariaDto;
+//	}
+//	
+//	static Secretaria criaSecretaria() {
+//
+//		Secretaria secretaria = new Secretaria();
+//		
+//		secretaria.setIdSecretaria(1L);
+//		secretaria.setAreaAtuacao(AreaAtuacao.SAUDE);
+//		secretaria.setOrcamentoProjeto(1000.0);
+//		secretaria.setOrcamentoFolha(1000.0);
+//		secretaria.setTelefone("34965847562");
+//		secretaria.setEndereco("Rua das Ostras, 123");
+//		secretaria.setSite("www.msmg.gov.br");
+//		secretaria.setEmail("msmg@gov.br");
+//		
+//		return secretaria;
+//	}
+//
+//}
