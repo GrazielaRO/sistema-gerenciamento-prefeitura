95e77dfdc5b1e7af86df6c37b87f2d6eab46b594
GrazielaRO <grazi375@hotmail.com>
2020-11-13 17:26:13 -0300

    correção de intentação

--- src/main/java/br/com/zup/estrelas/prefeitura/service/ProjetoService.java
+++ src/main/java/br/com/zup/estrelas/prefeitura/service/ProjetoService.java
@@ -15,10 +15,10 @@ import br.com.zup.estrelas.prefeitura.entity.Projeto;
 import br.com.zup.estrelas.prefeitura.entity.Secretaria;
 import br.com.zup.estrelas.prefeitura.repository.ProjetoRepository;
 import br.com.zup.estrelas.prefeitura.repository.SecretariaRepository;
- 
+
 @Service
-public class ProjetoService implements IProjetoService{
-	
+public class ProjetoService implements IProjetoService {
+
 	private static final String DESCRICAO_ALTERADA_COM_SUCESSO = "Descrição alterada com sucesso!";
 	private static final String DATA_FINAL_ANTERIOR_A_DATA_DE_INICIO = "A data final não pode ser anterior à data de início do projeto.";
 	private static final String ORCAMENTO_PARA_PROJETOS_INSUFICIENTE = "Orçamento para projetos insuficiente!";
@@ -26,57 +26,57 @@ public class ProjetoService implements IProjetoService{
 	private static final String POJETO_JA_CADASTRADO = "Pojeto já cadastrado!";
 	private static final String DATA_FINAL_ALTERADA_COM_SUCESSO = "Data final alterada com sucesso!";
 	private static final String PROJETO_INEXISTENTE = "Projeto inexistente";
-	
+
 	@Autowired
 	ProjetoRepository projetoRepository;
-	
+
 	@Autowired
 	SecretariaRepository secretariaRepository;
 
 	public MensagemDTO adicionaProjeto(ProjetoDTO projetoDto) {
-		
+
 		if (projetoRepository.findByNome(projetoDto.getNome()).isPresent()) {
 			return new MensagemDTO(POJETO_JA_CADASTRADO);
 		}
 
 		Optional<Secretaria> secretariaConsultada = secretariaRepository.findById(projetoDto.getIdSecretaria());
 		Secretaria secretaria = secretariaConsultada.get();
-		
+
 		if (secretaria.getOrcamentoProjeto() < projetoDto.getCusto()) {
 			return new MensagemDTO(ORCAMENTO_PARA_PROJETOS_INSUFICIENTE);
 		}
-		
+
 		Projeto projeto = new Projeto();
 		BeanUtils.copyProperties(projetoDto, projeto);
 		projeto.setDataEntrega(null);
 		projeto.setSecretaria(secretaria);
 		projetoRepository.save(projeto);
-		
+
 		secretaria.setOrcamentoProjeto(secretaria.getOrcamentoProjeto() - projetoDto.getCusto());
 		secretariaRepository.save(secretaria);
-		
+
 		return new MensagemDTO(PROJETO_CADASTRADO_COM_SUCESSO);
 	}
 
 	public Projeto consultaProjeto(Long idProjeto) {
-			return projetoRepository.findById(idProjeto).orElse(null);
+		return projetoRepository.findById(idProjeto).orElse(null);
 	}
 
 	public MensagemDTO alteraProjeto(Long idProjeto, AlteraProjetoDTO alteraprojetoDto) {
-		
+
 		Optional<Projeto> projetoConsultado = projetoRepository.findById(idProjeto);
-		
+
 		if (projetoConsultado.isEmpty()) {
-			return new MensagemDTO (PROJETO_INEXISTENTE);
+			return new MensagemDTO(PROJETO_INEXISTENTE);
 		}
-		
+
 		Projeto projetoAlterado = projetoConsultado.get();
-		
+
 		projetoAlterado.setDescricao(alteraprojetoDto.getDescricao());
-		
+
 		projetoRepository.save(projetoAlterado);
-		
-		return new MensagemDTO (DESCRICAO_ALTERADA_COM_SUCESSO);
+
+		return new MensagemDTO(DESCRICAO_ALTERADA_COM_SUCESSO);
 	}
 
 	public List<Projeto> listaProjetos() {
@@ -84,25 +84,25 @@ public class ProjetoService implements IProjetoService{
 	}
 
 	public MensagemDTO finalizaProjeto(Long idProjeto, DataFinalProjetoDTO dataFinal) {
-		
-		Optional <Projeto> projetoConsultado = projetoRepository.findById(idProjeto);
-		
+
+		Optional<Projeto> projetoConsultado = projetoRepository.findById(idProjeto);
+
 		if (projetoConsultado.isEmpty()) {
-			return new MensagemDTO (PROJETO_INEXISTENTE);
+			return new MensagemDTO(PROJETO_INEXISTENTE);
 		}
-		
+
 		Projeto projeto = projetoConsultado.get();
-		
+
 		if (projeto.getDataInicio().isAfter(dataFinal.getDataEntrega())) {
-			return new MensagemDTO (DATA_FINAL_ANTERIOR_A_DATA_DE_INICIO);
+			return new MensagemDTO(DATA_FINAL_ANTERIOR_A_DATA_DE_INICIO);
 		}
-		
+
 		projeto.setDataEntrega(dataFinal.getDataEntrega());
 		projeto.setConcluido(true);
-		
+
 		projetoRepository.save(projeto);
-		
-		return new MensagemDTO (DATA_FINAL_ALTERADA_COM_SUCESSO);
+
+		return new MensagemDTO(DATA_FINAL_ALTERADA_COM_SUCESSO);
 	}
 
 }
--- src/main/java/br/com/zup/estrelas/prefeitura/service/SecretariaService.java
+++ src/main/java/br/com/zup/estrelas/prefeitura/service/SecretariaService.java
@@ -13,47 +13,47 @@ import br.com.zup.estrelas.prefeitura.entity.Secretaria;
 import br.com.zup.estrelas.prefeitura.repository.SecretariaRepository;
 
 @Service
-public class SecretariaService implements ISecretariaService{
-	
+public class SecretariaService implements ISecretariaService {
+
 	private static final String ALTERACAO_REALIZADA_COM_SUCESSO = "Alteração realizada com sucesso!";
 	private static final String CADASTRO_REALIZADO_COM_SUCESSO = "Cadastro realizado com sucesso!";
 	private static final String SECRETARIA_JÁ_CADASTRADA = "Secretaria já cadastrada!";
 	private static final String SECRETARIA_REMOVIDA_COM_SUCESSO = "Secretaria removida com sucesso";
 	private static final String SECRETARIA_INEXISTENTE = "Secretaria inexistente.";
-	
+
 	@Autowired
 	SecretariaRepository secretariaRepository;
-	
-	public MensagemDTO adicionaSecretaria (CriaSecretariaDTO criaSecretariaDto) {
-		
+
+	public MensagemDTO adicionaSecretaria(CriaSecretariaDTO criaSecretariaDto) {
+
 		if (secretariaRepository.findByAreaAtuacao(criaSecretariaDto.getAreaAtuacao()).isPresent()) {
-			
+
 			return new MensagemDTO(SECRETARIA_JÁ_CADASTRADA);
 		}
-		
+
 		Secretaria secretaria = new Secretaria();
-		
+
 		BeanUtils.copyProperties(criaSecretariaDto, secretaria);
-		
+
 		secretariaRepository.save(secretaria);
 
-		return new MensagemDTO(CADASTRO_REALIZADO_COM_SUCESSO);	
+		return new MensagemDTO(CADASTRO_REALIZADO_COM_SUCESSO);
 	}
-	
-	public Secretaria consultarSecretaria (Long idSecretaria) {
+
+	public Secretaria consultarSecretaria(Long idSecretaria) {
 		return secretariaRepository.findById(idSecretaria).orElse(null);
 	}
-	
+
 	public MensagemDTO alteraSecretaria(Long idSecretaria, AlteraSecretariaDTO alteraSecretariaDTO) {
-		
+
 		Optional<Secretaria> secretariaConsultada = secretariaRepository.findById(idSecretaria);
-		
+
 		if (secretariaConsultada.isEmpty()) {
-			return new MensagemDTO (SECRETARIA_INEXISTENTE);
+			return new MensagemDTO(SECRETARIA_INEXISTENTE);
 		}
-		
+
 		Secretaria secretariaAlterada = secretariaConsultada.get();
-		
+
 		secretariaAlterada.setAreaAtuacao(alteraSecretariaDTO.getAreaAtuacao());
 		secretariaAlterada.setOrcamentoProjeto(alteraSecretariaDTO.getOrcamentoProjeto());
 		secretariaAlterada.setOrcamentoFolha(alteraSecretariaDTO.getOrcamentoFolha());
@@ -61,25 +61,25 @@ public class SecretariaService implements ISecretariaService{
 		secretariaAlterada.setEndereco(alteraSecretariaDTO.getEndereco());
 		secretariaAlterada.setSite(alteraSecretariaDTO.getSite());
 		secretariaAlterada.setEmail(alteraSecretariaDTO.getEmail());
-		
+
 		secretariaRepository.save(secretariaAlterada);
-		
-		return new MensagemDTO (ALTERACAO_REALIZADA_COM_SUCESSO);
+
+		return new MensagemDTO(ALTERACAO_REALIZADA_COM_SUCESSO);
 	}
-	
-	public MensagemDTO removeSecretaria (Long idSecretaria) {
-		
+
+	public MensagemDTO removeSecretaria(Long idSecretaria) {
+
 		if (!secretariaRepository.existsById(idSecretaria)) {
-			
-			return new MensagemDTO (SECRETARIA_INEXISTENTE);
+
+			return new MensagemDTO(SECRETARIA_INEXISTENTE);
 		}
-		
+
 		secretariaRepository.deleteById(idSecretaria);
-		
-		return new MensagemDTO (SECRETARIA_REMOVIDA_COM_SUCESSO);
+
+		return new MensagemDTO(SECRETARIA_REMOVIDA_COM_SUCESSO);
 	}
-	
-	public List<Secretaria> listaSecretarias(){
+
+	public List<Secretaria> listaSecretarias() {
 		return (List<Secretaria>) secretariaRepository.findAll();
 	}
 
